0b54cdb069c8553cfb785f79fb2c79da4ff95302
KyleFick <kylefick99@gmail.com>
2025-06-27 23:16:27 +0200

    Production build

--- .next/static/chunks/app_pages_aboutus_page_tsx_e2837d20._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_15251bc2._.js",
-  "static/chunks/_9946a87b._.js"
-],
-    source: "dynamic"
-});
